042a45002d17cd9d692e76a42d44f581be452b9b
songtianming <songtianming@huobi.com>
2019-11-14 16:06:27 +0800

    fix CreateTime UpdateTime type

--- model.go
+++ model.go
@@ -1,13 +1,11 @@
 package gorm
 
-import "time"
-
 // Model base model definition, including fields `ID`, `CreatedAt`, `UpdatedAt`, `DeletedAt`, which could be embedded in your models
 //    type User struct {
 //      gorm.Model
 //    }
 type Model struct {
-	ID         int64     `json:"id"          orm:"column(id)"`
-	CreateTime time.Time `json:"createTime"  orm:"column(create_time)"`
-	UpdateTime time.Time `json:"updateTime"  orm:"column(update_time)"`
+	ID         int64 `json:"id"          orm:"column(id)"`
+	CreateTime int64 `json:"createTime"  orm:"column(create_time)"`
+	UpdateTime int64 `json:"updateTime"  orm:"column(update_time)"`
 }
